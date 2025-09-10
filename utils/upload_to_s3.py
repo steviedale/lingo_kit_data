0cf56b6423515934ea8ce2a76063c129805550b1
Stevie Dale <sdale4@asu.edu>
2025-09-10 07:07:25 +0200

    fix base terms, add checking if file exists already to s3 upload

--- utils/upload_to_s3.py
+++ utils/upload_to_s3.py
@@ -13,6 +13,13 @@ def upload_file(file_path):
     bucket = 'steviedale-language-app' # Replace with your S3 bucket name
     object_name = os.path.basename(file_path)
 
+    # if file has already been uploaded, don't upload
+    url = f"https://{bucket}.s3.us-east-1.amazonaws.com/" + object_name
+    response = requests.head(url)
+    if response.status_code == 200:
+        print(f"file {object_name} already exists in bucket {bucket}, skipping upload")
+        return True
+
     s3_client = boto3.client('s3')
     try:
         s3_client.upload_file(file_path, bucket, object_name)
