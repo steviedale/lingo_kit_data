68217ea70439ec2e81ba26adc551b48469553f2d
Stevie Dale <sdale4@asu.edu>
2025-09-28 23:11:03 +0200

    wip

---
+++ dataframes/v1.1.1/csv_helper.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+
+
+def get_all_csv_files_rec(dir):
+    """Ritorna una lista di tutti i file .csv nella directory e nelle sottodirectory."""
+    csv_files = []
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if file.endswith('.csv'):
+                csv_files.append(os.path.join(root, file))
+    return csv_files
+
+
+def get_all_terms_df():
+    all_csv_files = get_all_csv_files_rec('/Users/stevie/repos/lingo_kit_data/dataframes/v1.1.1/dataframes')
+    all_df = pd.DataFrame()
+    for path in all_csv_files:
+        assert(os.path.exists(path))
+        # print(path)
+        df = pd.read_csv(path)
+        all_df = pd.concat([all_df, df], ignore_index=True)
+    # len(all_df), all_df.columns
+    return all_df
