6c1162db66775b141014be3bcca5422b3b341e20
Stevie Dale <sdale4@asu.edu>
2025-10-08 11:13:57 +0200

    wip, in the middle of creating csv files for spotify terms, moving to AWS

---
+++ dataframes/generate_csv_data.py
@@ -0,0 +1,93 @@
+# %%
+import os
+from tqdm import tqdm
+import pandas as pd
+import sys
+sys.path.append('/Users/stevie/repos/lingo_kit_data/')
+from utils.chatgpt.generate_with_chatgpt import generate_csv
+from utils.csv_helper import get_all_terms_df
+
+# %%
+path = '/Users/stevie/repos/lingo_kit_data/dataframes/old_dataframes/spotify_lessons.csv'
+assert(os.path.exists(path))
+df = pd.read_csv(path)
+len(df), df.columns
+
+# %%
+punctuations = '''!()-[]{};:"\,<>./?@#$%^&*_~'''
+word_set = set()
+for i, row in tqdm(df.iterrows(), total=len(df)):
+    words = row['italian_term'].lower().split(' ')
+    for word in words:
+        for p in punctuations:
+            word = word.replace(p, '')
+        if word != '':
+            word_set.add(word)
+len(word_set)
+
+# %%
+italian_words = list(sorted(word_set))
+
+# %%
+new_list = []
+all_df = get_all_terms_df()
+for term_it in tqdm(italian_words):
+    if term_it.lower() in all_df['term_italian'].str.lower().values:
+        print(f"Skipping term '{term_it}' as it already exists in the dataset.")
+    else:
+        new_list.append(term_it)
+len(new_list)
+
+# %%
+italian_words = sorted(new_list)
+
+# %%
+print(len(italian_words))
+italian_words = [x for x in italian_words if 'mille' not in x]
+print(len(italian_words))
+
+# %%
+blacklist = ['guardia']
+
+# %%
+response_list = []
+generated_files_list = []
+cost_info_list = []
+for term_it in tqdm(italian_words):
+    if term_it in blacklist:
+        print(f"Skipping blacklisted term '{term_it}'.")
+        continue
+
+    all_df = get_all_terms_df()
+    if term_it.lower() in all_df['term_italian'].str.lower().values:
+        print(f"Skipping term '{term_it}' as it already exists in the dataset.")
+        continue
+
+    attempt_count = 0
+    for attempt_count in range(3):
+        try:
+            print(f"Attempt {attempt_count + 1} for term: {term_it}")
+            response, generated_files, cost_info = generate_csv(
+                italian_term=term_it,
+                model='gpt-5-mini',
+                # model='gpt-5',
+                reasoning_effort='medium',
+            )
+            response_list.append(response)
+            generated_files_list.extend(generated_files)
+            cost_info_list.append(cost_info)
+            print(f"cost of this call: {cost_info['total_cost']}")
+            break  # Exit the retry loop if successful
+        except pd.errors.ParserError as e:
+            print(f"ParserError on attempt {attempt_count + 1} for term '{term_it}': {e}")
+            if attempt_count == 2:
+                print(f"Failed to process term '{term_it}' after 3 attempts.")
+        except Exception as e:
+            print(f"Error on attempt {attempt_count + 1} for term '{term_it}': {e}")
+            if attempt_count == 2:
+                print(f"Failed to process term '{term_it}' after 3 attempts.")
+
+# %%
+
+
+
---
+++ generate_audio/generate_audio_for_indiv_csvs.py
@@ -0,0 +1,110 @@
+# %%
+import os
+import pandas as pd
+from tqdm import tqdm
+import yaml
+import requests
+import hashlib
+import uuid
+
+import sys
+sys.path.append("/Users/stevie/repos/lingo_kit_data")
+from utils.audio.text_to_speech import TextToSpeech, VOICES
+from utils.s3.upload_to_s3 import upload_file
+from utils.csv_helper import get_all_csv_files_rec
+
+# %%
+df_path = '/Users/stevie/repos/lingo_kit_data/dataframes/dataframes_by_pos'
+all_csv_files = get_all_csv_files_rec(df_path)
+
+# %%
+tts = TextToSpeech()
+
+# %%
+def get_term_hash(english_text, italian_text):
+    hash = str(uuid.uuid5(uuid.NAMESPACE_DNS, f"{english_text}-{italian_text}"))
+    return hash
+print(get_term_hash('person', 'persone'))
+
+# %%
+def parse_topics(topics_str):
+    assert(topics_str[0] == '[' and topics_str[-1] == ']')
+    topics_str = topics_str[1:-1]  # remove brackets
+    topics = topics_str.split(',')
+    return topics
+
+def get_language_key(lang):
+    if lang == 'english':
+        return 'en'
+    elif lang == 'italian':
+        return 'it'
+    else:
+        raise ValueError(f"Unknown language: {lang}")
+
+def get_audio_hash(text, voice_name, speaking_rate, pitch):
+    param_str = f"{text}{voice_name}{speaking_rate:.2f}{pitch:.2f}"
+    hash_object = hashlib.sha256(param_str.encode('utf-8'))
+    hash_key = hash_object.hexdigest()
+    return hash_key
+
+# %%
+def is_csv_processed(df):
+    for lang in 'english', 'italian':
+        if f'{lang}_audio_hash' not in df.columns:
+            return False
+        if f"{lang}_duration_ms" not in df.columns:
+            return False
+        if sum(df[f'{lang}_audio_hash'].isna()) > 0:
+            return False
+        if sum(df[f'{lang}_duration_ms'].isna()) > 0:
+            return False
+    return True
+
+for csv_path in tqdm(all_csv_files):
+    if not os.path.exists(csv_path):
+        print(f"File no longer exists, skipping: {csv_path}")
+        continue
+    print(f"Processing file: {csv_path}")
+    assert(os.path.exists(csv_path)), f"File does not exist: {csv_path}"
+    df = pd.read_csv(csv_path)
+    if is_csv_processed(df):
+        print(f"File already processed: {csv_path}")
+        continue
+    for i, (index, row) in enumerate(df.iterrows()):
+        for lang in 'english', 'italian':
+            if lang == 'english':
+                gender = 'female'
+                speaking_rate = 0.92
+                text = row['translation_english']
+            else:
+                assert(lang == 'italian')
+                gender = 'male'
+                speaking_rate = 0.7
+                text = row['term_italian']
+
+            # this is a fix to make short words like "a" or "the" followed by parenthesis to sound too short
+            synth_obj = tts.synthesize(
+                text=text,
+                voice_name=VOICES[lang][gender],
+                speaking_rate=speaking_rate,
+                verbose=False,
+            )
+
+            local_hash = get_audio_hash(
+                text=text,
+                voice_name=VOICES[lang][gender],
+                speaking_rate=speaking_rate,
+                pitch=synth_obj['pitch'],
+            )
+            assert(local_hash == synth_obj['hash']), f"Hash mismatch: {local_hash} != {synth_obj['hash']}"
+
+            # upload audio file to s3
+            file_path = os.path.join('/Users/stevie/repos/lingo_kit_data', synth_obj['audio_file'])
+            assert(os.path.exists(file_path)), f"File does not exist: {file_path}"
+            upload_file(file_path=file_path, verbose=False)
+
+            df.loc[index, f'{lang}_audio_hash'] = local_hash
+            df.loc[index, f'{lang}_duration_ms'] = synth_obj['duration_ms']
+        print(f"Processing {i+1}/{len(df)}: {row['term_italian']} / {row['translation_english']} -> {df.loc[index, 'italian_audio_hash']} / {df.loc[index, 'english_audio_hash']}")
+    df.to_csv(csv_path, index=False)
+    tts.save()
